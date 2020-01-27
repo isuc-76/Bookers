bc7906b4b610d776e6c1a40daa782f4550d4f18a
isuc-76/76 <eriasimoyase@gmail.com>
2020-01-27 19:24:28 +0900

    toko

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,8 @@
 class BooksController < ApplicationController
+  
+  def top
+  end
+
   def index
   end
 
@@ -6,8 +10,22 @@ class BooksController < ApplicationController
   end
 
   def new
+  	@book = Book.new
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to '/books'
   end
 
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
