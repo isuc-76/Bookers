5b72a3159a95db105d9302ff16479d2243542010
isuc-76/76 <eriasimoyase@gmail.com>
2020-01-28 17:05:20 +0900

    henkoumae

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
   end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
   def new
@@ -21,6 +22,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   private
