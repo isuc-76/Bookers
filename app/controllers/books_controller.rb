469157025e60cff790832eee0edb48cdee278f4f
isuc-76/76 <eriasimoyase@gmail.com>
2020-01-29 19:39:19 +0900

    kinou ok??

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,7 +19,8 @@ class BooksController < ApplicationController
   def create
   	@book = Book.new(book_params)
   	if @book.save
-  	redirect_to books_path
+  		flash[:notice] = "Book was successfully created."
+  	redirect_to book_path(@book.id)
     else
 	@books = Book.all
 	render action: :index
@@ -32,7 +33,8 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update(book_params)
+    if
+    @book.update(book_params)
     redirect_to book_path(@book.id)
 	else
 		render action: :edit
