824833c511761e7c401c63b1e3e68b6d04e25eae
isuc-76/76 <eriasimoyase@gmail.com>
2020-01-29 17:57:58 +0900

    edit kansei

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,11 +31,10 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id)
 	else
-		@book = Book.find(params[:id])
 		render action: :edit
 	end
   end
